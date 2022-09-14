b6b566e01693cd540d5fb6adb28eff8920363cad
iamoen <owen.corona6@gmail.com>
2022-09-14 20:51:59 +0800

    Added div to show result and score

--- script.js
+++ script.js
@@ -37,17 +37,54 @@ function playRound(player,computer){
             break;
         default:
             result = "Ooops that's Invalid";
+            
             break;
     }
     return result;
 }
 
 function game(){
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Janken....");
-        let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection,computerSelection));
+    const container = document.createElement('div');
+    const rockButton = document.createElement('button');
+    const scissorsButton = document.createElement('button');
+    const paperButton = document.createElement('button');
+    const result = document.createElement('div');
+    
+    document.body.appendChild(container);
+    container.appendChild(rockButton);
+    container.appendChild(scissorsButton);
+    container.appendChild(paperButton);
+    document.body.appendChild(result);
+    
+    rockButton.id = "rock";
+    scissorsButton.id = "scissors";
+    paperButton.id = "paper";
+    result.id = "scoreboard";
+
+    rockButton.innerHTML = "Rock";
+    scissorsButton.innerHTML = "Scissors";
+    paperButton.innerHTML = "Paper";
+
+    let r = document.getElementById("rock").innerHTML;
+    let s = document.getElementById("scissors").innerHTML;
+    let p = document.getElementById("paper").innerHTML;
+    
+    const click_result = function(){
+        let score_ = [0,0];
+        let res = playRound(this.id, getComputerChoice());
+        
+        if(res.includes("WIN")){
+            score_[0]++;
+        }else if(res.includes("LOSE")){
+            score_[1]++;
+        }
+        result.innerHTML = res + " " + score_[0] + " - " + score_[1];
     }
+
+    rockButton.onclick = click_result;
+    scissorsButton.onclick = click_result;
+    paperButton.onclick = click_result;
+
 }
 
 game();
